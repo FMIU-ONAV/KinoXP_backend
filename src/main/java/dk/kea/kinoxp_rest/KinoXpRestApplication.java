dfea6b7f63f01a02e7cb8f8896a87adace8e8efe
Teller501 <ande777j@stud.kea.dk>
2023-10-02 09:20:23 +0200

    initial commit

---
+++ src/main/java/dk/kea/kinoxp_rest/KinoXpRestApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.kinoxp_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/kinoxp_rest/KinoXpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.kinoxp_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
